061006e3bb4fa0e04d4060060bdbc37073b3394e
tjdi <1335751696@qq.com>
2019-11-29 10:55:34 +0800

    11/29 10:55 first

---
+++ src/test/java/com/example/repository/StudentRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentRepositoryTest {
+
+    @Before
+    public void before(){
+    }
+
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findByTeacherID() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void select() {
+    }
+
+    @After
+    public void after(){
+    }
+}
